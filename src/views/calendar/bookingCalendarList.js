0f0bffdb01a76c950ef2e58c1e1c2b2427ac4dd3
Valentinaga1 <valentinagoa2@gmail.com>
2021-12-17 16:29:49 -0500

    Adding calendar events

---
+++ src/views/calendar/bookingCalendarList.js
@@ -0,0 +1,197 @@
+// @packages
+import React, { useState, useEffect, useContext } from 'react';
+import moment from 'moment';
+import { Col, Spinner } from 'reactstrap';
+import { useQuery, useLazyQuery } from '@apollo/client';
+
+// @scripts
+import BookingsHeader from '../booking/BookingsHeader/BookingsHeader';
+import Calendar from './calendar';
+import queryAllBookings from '../../graphql/QueryAllBookings';
+import queryAllCalendarEvents from '../../graphql/QueryAllCalendarEvents';
+import queryAllClasses from '../../graphql/QueryAllClasses';
+import queryAllCoordinators from '../../graphql/QueryAllEventCoordinators';
+import queryAllCustomers from '../../graphql/QueryAllCustomers';
+import { FiltersContext } from '../../context/FiltersContext/FiltersContext';
+import { getCustomerEmail, getClassTitle } from '../booking/common';
+
+const BookingCalendarList = () => {
+  const excludedBookings = ['closed', 'canceled'];
+  const genericFilter = {};
+  const [bookingsFilter, setBookingsFilter] = useState({ status_nin: excludedBookings });
+  const [bookings, setBookings] = useState([]);
+  const [limit, setLimit] = useState(20000);
+  const [customers, setCustomers] = useState([]);
+  const [coordinators, setCoordinators] = useState([]);
+  const [classes, setClasses] = useState([]);
+  const [calendarEvents, setCalendarEvents] = useState([]);
+  const [switchView, setSwitchView] = useState(false);
+  const [showFiltersModal, setShowFiltersModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [currentElement, setCurrentElement] = useState({});
+  const [elementToAdd, setElementToAdd] = useState({});
+  const { classFilterContext, coordinatorFilterContext, textFilterContext, dateFilterContext } = useContext(FiltersContext);
+  const [filteredBookings, setFilteredBookings] = useState([]);
+  const [editModal, setEditModal] = useState(false);
+
+  const handleEditModal = () => setEditModal(!editModal);
+
+  const [getBookings, { ...allBookingsResult }] = useLazyQuery(queryAllBookings, {
+    fetchPolicy: 'no-cache',
+    pollInterval: 200000,
+    onCompleted: (data) => {
+      if (data) setBookings(data.bookings.map((element) => element));
+    }
+  });
+
+  const [getCalendarEvents, { ...allCalendarEventsResults }] = useLazyQuery(queryAllCalendarEvents, {
+    fetchPolicy: 'no-cache',
+    onCompleted: (data) => {
+      if (data) {
+        setCalendarEvents(data.calendarEvents);
+        getBookings({
+          variables: {
+            filter: bookingsFilter,
+            limit
+          }
+        });
+      }
+    }
+  });
+
+  const { ...allCustomersResult } = useQuery(queryAllCustomers, {
+    fetchPolicy: 'no-cache',
+    variables: {
+      filter: genericFilter
+    },
+    onCompleted: (data) => {
+      if (data) setCustomers(data.customers);
+    },
+    pollInterval: 200000
+  });
+
+  const { ...allCoordinatorResult } = useQuery(queryAllCoordinators, {
+    variables: {
+      filter: genericFilter
+    },
+    onCompleted: (data) => {
+      if (data) setCoordinators(data.eventCoordinators);
+    },
+    pollInterval: 200000
+  });
+
+  const { ...allClasses } = useQuery(queryAllClasses, {
+    pollInterval: 200000,
+    variables: {
+      filter: genericFilter
+    },
+    onCompleted: (data) => {
+      if (data && data.teamClasses) {
+        setClasses(data.teamClasses);
+        getCalendarEvents({
+          variables: {
+            filter: genericFilter
+          }
+        });
+      }
+    }
+  });
+
+  const handleModal = () => setShowAddModal(!showAddModal);
+
+  const handleSearch = (value) => {
+    if (value.length) {
+      const updatedData = bookings.filter((item) => {
+        const startsWith =
+          (item.customerName && item.customerName.toLowerCase().startsWith(value.toLowerCase())) ||
+          (item.customerId && getCustomerEmail(item.customerId, customers).toLowerCase().startsWith(value.toLowerCase())) ||
+          (item.teamClassId && getClassTitle(item.teamClassId, classes).toLowerCase().startsWith(value.toLowerCase())) ||
+          item._id.startsWith(value);
+
+        const includes =
+          (item.customerName && item.customerName.toLowerCase().includes(value.toLowerCase())) ||
+          (item.customerId && getCustomerEmail(item.customerId, customers).toLowerCase().includes(value.toLowerCase())) ||
+          (item.teamClassId && getClassTitle(item.teamClassId, classes).toLowerCase().includes(value.toLowerCase())) ||
+          item._id.includes(value);
+
+        return startsWith || includes;
+      });
+
+      setFilteredBookings(updatedData);
+    } else {
+      setFilteredBookings(bookings);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch((textFilterContext && textFilterContext.value) || '');
+  }, [bookings]);
+
+  useEffect(() => {
+    let query = {
+      status_nin: excludedBookings
+    };
+
+    if (classFilterContext) {
+      query = { ...query, teamClassId: classFilterContext.value };
+    }
+
+    if (coordinatorFilterContext) {
+      query = { ...query, eventCoordinatorId_in: coordinatorFilterContext.value };
+    }
+
+    if (dateFilterContext) {
+      query = {
+        ...query,
+        createdAt_gte: moment(dateFilterContext.value[0]).format(),
+        createdAt_lte: moment(dateFilterContext.value[1]).add(23, 'hours').add(59, 'minutes').format()
+      };
+    }
+
+    setBookingsFilter(query);
+  }, [classFilterContext, coordinatorFilterContext, dateFilterContext]);
+
+  useEffect(() => {
+    handleSearch((textFilterContext && textFilterContext.value) || '');
+  }, [textFilterContext]);
+
+  useEffect(() => {
+    if (calendarEvents && customers && classes) getBookings({
+      variables: {
+        filter: bookingsFilter,
+        limit
+      }
+    });
+  }, [bookingsFilter, limit]);
+
+  // ** Function to handle Modal toggle
+  return (
+    <>
+      <p>Calendar</p>
+      <BookingsHeader
+        setShowFiltersModal={(val) => setShowFiltersModal(val)}
+        showAddModal={() => handleModal()}
+        setElementToAdd={(d) => setElementToAdd(d)}
+        onChangeLimit={(newLimit) => {
+          setLimit(newLimit);
+        }}
+        bookings={filteredBookings}
+        customers={customers}
+        coordinators={coordinators}
+        classes={classes}
+        calendarEvents={calendarEvents}
+        defaultLimit={limit}
+        showLimit={true}
+        showExport={true}
+        showAdd={false}
+        showFilter={true}
+        showView={false}
+        titleView={'Events Calendar '}
+        isClosedBookings={true}
+      />
+      <Calendar bookings={bookings} calendarEvents={calendarEvents} classes={classes} customers={customers} />
+    </>
+  );
+};
+
+export default BookingCalendarList;
---
+++ src/views/calendar/calendar.js
@@ -0,0 +1,160 @@
+// ** React Import
+import React, { useState, useEffect } from 'react';
+
+// ** Full Calendar & it's Plugins
+import FullCalendar from '@fullcalendar/react';
+import listPlugin from '@fullcalendar/list';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import { Button, Modal, ModalBody, ModalHeader, ModalFooter, Alert } from 'reactstrap';
+import moment from 'moment';
+import './calendar.scss';
+import { BOOKING_DATE_REQUESTED_STATUS } from '../../utility/Constants';
+import {
+  getClassTitle,
+  getFormattedEventDate,
+  getCustomerName,
+  getCustomerCompany,
+  getCustomerPhone,
+  getCustomerEmail,
+  getFormattedEventDate2
+} from '../booking/common';
+
+const Calendar = ({ bookings, calendarEvents, classes, customers }) => {
+  const [events, setEvents] = useState([]);
+  const [filteredCalendarEvents, setFilteredCalendarEvents] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalClassTitle, setModalClassTitle] = useState(null);
+  const [modalBookingInfo, setModalBookingInfo] = useState(null);
+
+  const handleClose = () => setShowModal(false);
+
+  useEffect(() => {
+    if (calendarEvents) {
+      const confirmedCalendarEvents = calendarEvents.filter((item) => item.status === 'confirmed');
+      setFilteredCalendarEvents(confirmedCalendarEvents);
+    }
+  }, [calendarEvents]);
+
+  console.log('filteredCalendarEvents', filteredCalendarEvents);
+
+  useEffect(() => {
+    if (bookings && filteredCalendarEvents && classes) {
+      const eventsArray = [];
+      bookings.map((item, index) => {
+        const formatDate = getFormattedEventDate(item._id, filteredCalendarEvents);
+        eventsArray.push({
+          title: `${
+            getCustomerCompany(item.customerId, customers) ? getCustomerCompany(item.customerId, customers).concat(' / ') : ''
+          }${getCustomerName(item.customerId, customers)}`,
+          classTitle: getClassTitle(item.teamClassId, classes),
+          bookingId: item._id,
+          attendees: item.attendees,
+          customerName: getCustomerName(item.customerId, customers),
+          customerCompany: getCustomerCompany(item.customerId, customers),
+          customerPhone: getCustomerPhone(item.customerId, customers),
+          customerEmail: getCustomerEmail(item.customerId, customers),
+          classVariant:
+            item.classVariant && `${item.classVariant.title} $${item.classVariant.pricePerson}${item.classVariant.groupEvent ? '/group' : '/person'}`,
+          signUpDeadline: item.signUpDeadline,
+          eventDate: getFormattedEventDate(item._id, filteredCalendarEvents),
+          date: moment(getFormattedEventDate(item._id, filteredCalendarEvents), 'LL').format('YYYY-MM-DD HH:mm'),
+          backgroundColor: item.status === BOOKING_DATE_REQUESTED_STATUS ? '#FF6563' : '#557FE7',
+          status: item.status
+        });
+        setEvents(eventsArray);
+      });
+    }
+  }, [bookings, filteredCalendarEvents, classes]);
+  console.log(events);
+
+  return (
+    <>
+      <Modal
+        isOpen={showModal}
+        toggle={() => setShowModal(!showModal)}
+        className="sidebar-sm"
+        onClosed={(e) => handleClose()}
+        onClick={(e) => e.preventDefault()}
+      >
+        <ModalHeader color="primary">
+          <span className="font-weight-bolder">{modalClassTitle}</span>
+        </ModalHeader>
+        <ModalBody>
+          <p>
+            <strong>Name: </strong>
+            {modalBookingInfo && modalBookingInfo.customerName}
+          </p>
+          {modalBookingInfo && modalBookingInfo.customerCompany && (
+            <p>
+              <strong>Company: </strong>
+              {modalBookingInfo.customerCompany}
+            </p>
+          )}
+          <p>
+            <strong>Class: </strong>
+            {modalBookingInfo && modalBookingInfo.classTitle}
+          </p>
+          <p>
+            <strong>Event date: </strong>
+            {modalBookingInfo && modalBookingInfo.eventDate}
+          </p>
+          <p>
+            <strong>Class Variant: </strong>
+            {modalBookingInfo && modalBookingInfo.classVariant}
+          </p>
+          <p>
+            <strong>Attendees: </strong>
+            {modalBookingInfo && modalBookingInfo.attendees}
+          </p>
+          {modalBookingInfo && modalBookingInfo.signUpDeadline && (
+            <p>
+              <strong>Sign Up Deadline: </strong>
+              {moment(modalBookingInfo.signUpDeadline).format('lll')}
+            </p>
+          )}
+          <Alert color="danger" isOpen={modalBookingInfo && modalBookingInfo.status === BOOKING_DATE_REQUESTED_STATUS ? true : false}>
+            <span>Date and time was accepted but event is yet to be confirmed.</span>
+          </Alert>
+        </ModalBody>
+        <ModalFooter className="d-flex justify-content-center">
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              setShowModal(!showModal);
+            }}
+            color="primary"
+            size="sm"
+            className="col-2 "
+          >
+            Ok
+          </Button>{' '}
+        </ModalFooter>
+      </Modal>
+
+      <div className="calendar font-weight-normal small ">
+        <FullCalendar
+          defaultView="dayGridMonth"
+          dayMaxEvents={3}
+          plugins={[dayGridPlugin, listPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          eventClick={(e) => {
+            setShowModal(!showModal);
+            setModalClassTitle(e.event._def.title);
+            setModalBookingInfo(e.event._def.extendedProps);
+          }}
+          events={{ events }}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Calendar;
