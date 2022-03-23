fb893a352593883672eb481b152787c07918fc37
Valentinaga1 <valentinagoa2@gmail.com>
2022-03-23 16:32:09 -0500

    Removing aws-export file from git ignore

---
+++ src/aws-exports.js
@@ -0,0 +1,17 @@
+const awsmobile = {
+  aws_project_region: "us-east-1",
+  aws_cognito_identity_pool_id: "us-east-1:f46e5776-fad5-431d-a159-211c4bd281c6",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_w6wXuRnsC",
+  aws_user_pools_web_client_id: "7g1pnenmn0ttul7h33g53ai65a",
+  oauth: {},
+  aws_user_files_s3_bucket: "teamclasss3131931-prod",
+  aws_user_files_s3_bucket_region: "us-east-1",
+  aws_appsync_graphqlEndpoint: "https://be6nmwpagjcsbpcpfxs7xwpi3a.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-1",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-hnpij72go5e4jirkgnyihqlvzm"
+};
+
+
+export default awsmobile;
