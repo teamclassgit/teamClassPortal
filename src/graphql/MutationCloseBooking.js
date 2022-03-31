b362b81e9817c6d8f3cdc80e9b6f97bbb45b4cde
Valentinaga1 <valentinagoa2@gmail.com>
2022-03-31 12:43:55 -0500

    Fixing mutation toCloseBooking to change all the information in the booking

--- src/graphql/MutationCloseBooking.js
+++ src/graphql/MutationCloseBooking.js
@@ -1,8 +1,105 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-  mutation updateBookingStatusToClose($bookingId: String!, $status: String!, $updatedAt: DateTime!, $closedReason: String!) {
-    updateOneBooking(query: { _id: $bookingId }, set: { status: $status, closedReason: $closedReason, updatedAt: $updatedAt }) {
+  mutation updateBookingStatusToClose(
+    $bookingId: String!
+    $date: DateTime!
+    $teamClassId: String!
+    $classVariant: BookingClassVariantUpdateInput!
+    $notes: [BookingNoteUpdateInput!]
+    $instructorId: String
+    $instructorName: String
+    $customerId: String!
+    $customerName: String!
+    $eventDate: DateTime!
+    $eventDurationHours: Float
+    $eventCoordinatorId: String!
+    $attendees: Int!
+    $classMinimum: Int!
+    $pricePerson: Float!
+    $salesTax: Float!
+    $serviceFee: Float!
+    $discount: Float!
+    $createdAt: DateTime!
+    $updatedAt: DateTime!
+    $signUpDeadline: DateTime
+    $status: String!
+    $phone: String!
+    $email: String!
+    $company: String
+    $closedReason: String
+    $capRegistration: Boolean
+    $joinInfo: BookingJoinInfoUpdateInput
+    $joinInfo_unset: Boolean
+    $shippingTrackingLink: String
+  ) {
+    updateOneCustomer(
+      query: { _id: $customerId }
+      set: { _id: $customerId, name: $customerName, phone: $phone, email: $email, company: $company, updatedAt: $updatedAt }
+    ) {
+      _id
+      name
+      email
+      phone
+      company
+      billingAddress {
+        addressLine1
+        addressLine2
+        city
+        state
+        country
+        zip
+      }
+      createdAt
+      updatedAt
+    }
+    updateOneCalendarEvent(query: { bookingId: $bookingId }, set: { classId: $teamClassId }) {
+      _id
+      classId
+      bookingId
+      year
+      month
+      day
+      fromHour
+      fromMinutes
+      toHour
+      toMinutes
+      status
+      rushFee
+    }
+    updateOneBooking(
+      query: { _id: $bookingId }
+      set: {
+        _id: $bookingId
+        date: $date
+        expirationHours: 48
+        classVariant: $classVariant
+        notes: $notes
+        teamClassId: $teamClassId
+        instructorId: $instructorId
+        instructorName: $instructorName
+        customerId: $customerId
+        customerName: $customerName
+        eventDate: $eventDate
+        eventDurationHours: $eventDurationHours
+        eventCoordinatorId: $eventCoordinatorId
+        attendees: $attendees
+        classMinimum: $classMinimum
+        pricePerson: $pricePerson
+        serviceFee: $serviceFee
+        salesTax: $salesTax
+        discount: $discount
+        status: $status
+        createdAt: $createdAt
+        updatedAt: $updatedAt
+        signUpDeadline: $signUpDeadline
+        closedReason: $closedReason
+        capRegistration: $capRegistration
+        joinInfo: $joinInfo
+        joinInfo_unset: $joinInfo_unset
+        shippingTrackingLink: $shippingTrackingLink
+      }
+    ) {
       _id
       teamClassId
       customerId
@@ -18,6 +115,7 @@ export default gql`
       discount
       status
       closedReason
+      capRegistration
       eventLink
       signUpStatusLink
       checkoutLink
@@ -45,6 +143,13 @@ export default gql`
       createdAt
       updatedAt
       signUpDeadline
+      shippingTrackingLink
+      joinInfo {
+        eventId
+        joinUrl
+        manualLink
+        password
+      }
     }
   }
 `;
