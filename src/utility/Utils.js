c3c9d489ef952ce2858042add7065cc0a3614174
Carlos Montoya <carlos@teamclass.com>
2022-11-23 11:29:16 -0500

    eslint fix

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -16,8 +16,9 @@ import { isAnon, userData } from "./RealmApolloClient";
 export const isObjEmpty = (obj) => Object.keys(obj).length === 0;
 
 // ** Returns K format from a number
-export const kFormatter = (num) =>
-  num > 999 ? `${(num / 1000).toFixed(1)}k` : num;
+export const kFormatter = (num) => {
+  return num > 999 ? `${(num / 1000).toFixed(1)}k` : num;
+};
 
 // ** Converts HTML to string
 export const htmlToString = (html) => html.replace(/<\/?[^>]+(>|$)/g, "");
